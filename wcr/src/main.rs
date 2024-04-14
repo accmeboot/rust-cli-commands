b79c1ed4fb87eb86d57f9c9d4ed6adde3dd9e1d3
= <=>
2024-04-14 20:13:32 +0200

    feat: wcr implementation

---
+++ wcr/src/main.rs
@@ -0,0 +1,175 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILE", default_value = "-")]
+    files: Vec<String>,
+
+    /// Show line count
+    #[arg(short, long)]
+    lines: bool,
+
+    /// Show word count
+    #[arg(short, long)]
+    words: bool,
+
+    /// Show byte count
+    #[arg(short('c'), long)]
+    bytes: bool,
+
+    /// Show character count
+    #[arg(short('m'), long, conflicts_with("bytes"))]
+    chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
+fn count(mut file: impl BufRead) -> Result<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+
+    let mut line = String::new();
+
+    loop {
+        let line_bytes = file.read_line(&mut line)?;
+        if line_bytes == 0 {
+            break;
+        }
+
+        num_bytes += line_bytes;
+        num_lines += 1;
+        num_words += line.split_whitespace().count();
+        num_chars += line.chars().count();
+
+        line.clear();
+    }
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn format_field(value: usize, show: bool) -> String {
+    if show {
+        format!("{value:>8}")
+    } else {
+        "".to_string()
+    }
+}
+
+fn run(mut args: Args) -> Result<()> {
+    if [args.words, args.bytes, args.chars, args.lines]
+        .iter()
+        .all(|v| !v)
+    {
+        args.lines = true;
+        args.words = true;
+        args.bytes = true;
+    }
+
+    let mut total_lines = 0;
+    let mut total_words = 0;
+    let mut total_bytes = 0;
+    let mut total_chars = 0;
+
+    for filename in &args.files {
+        match open(filename) {
+            Err(err) => eprintln!("Failed opening a {filename}: {err}"),
+            Ok(file) => {
+                let file_info = count(file)?;
+
+                println!(
+                    "{}{}{}{}{}",
+                    format_field(file_info.num_lines, args.lines),
+                    format_field(file_info.num_words, args.words),
+                    format_field(file_info.num_bytes, args.bytes),
+                    format_field(file_info.num_chars, args.chars),
+                    if filename == "-" {
+                        "".to_string()
+                    } else {
+                        format!(" {filename}")
+                    }
+                );
+
+                total_lines += file_info.num_lines;
+                total_words += file_info.num_words;
+                total_bytes += file_info.num_bytes;
+                total_chars += file_info.num_chars;
+            }
+        }
+    }
+
+    if args.files.len() > 1 {
+        println!(
+            "{}{}{}{} total",
+            format_field(total_lines, args.lines),
+            format_field(total_words, args.words),
+            format_field(total_bytes, args.bytes),
+            format_field(total_chars, args.chars),
+        );
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run(Args::parse()) {
+        eprintln!("{e}");
+        std::process::exit(1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{count, format_field, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world.\nI just want your half.\r\n";
+        let info = count(Cursor::new(text));
+
+        assert!(info.is_ok());
+        assert_eq!(
+            info.unwrap(),
+            FileInfo {
+                num_lines: 2,
+                num_words: 10,
+                num_chars: 48,
+                num_bytes: 48,
+            }
+        );
+    }
+
+    #[test]
+    fn test_format_field() {
+        assert_eq!(format_field(1, false), "");
+        assert_eq!(format_field(3, true), "       3");
+        assert_eq!(format_field(10, true), "      10");
+    }
+}
